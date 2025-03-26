a799d0a25b7d1fa35f33c7f156c4747da00330c9
Thomas Papamichail <thomas@pointer.gr>
2025-03-26 19:44:21 +0200

    Remove build folder from Git tracking

---
+++ build/pointer-domain-search/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5ccf546b2ed7df4fb108');
---
+++ build/pointer-domain-search/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd0ae7feaf36483356779');
